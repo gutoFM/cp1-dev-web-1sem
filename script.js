b35b433a9860a35c668ae0f0f15273a0a1c61b7c
gutoFM <gutofisco@gmail.com>
2023-04-04 20:28:33 -0300

    Questao 4

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ if(imc < 18.5){
     texto3 = "Você está acima do peso"
     console.log(texto3)
 }
+
+//Questão 4
+let idade = parseInt(prompt("Digite sua idade: "))
+
+if(idade <= 12){
+    console.log("Você é uma criança.")
+} else if(idade <= 18){
+    console.log("Você é um adolescente.")
+} else if(idade <= 60){
+    console.log("Você é um adulto.")
+} else{
+    console.log("Você é um idoso.")
+}
