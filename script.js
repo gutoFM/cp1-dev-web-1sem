261bb45c4426cfb97f3c6c4e9682475e2ea42219
gutoFM <gutofisco@gmail.com>
2023-04-04 20:20:43 -0300

    Questao 3

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ console.log(fiap);
 
 //Questão 2
 let num = 12 % 5
-console.log(num)
+console.log(num);
+
+//Questão 3
+let peso = parseFloat(prompt("Digite seu peso em KG(EX: 71,5) : "));
+let altura = parseFloat(prompt("Digite sua altura em metrose o restante em centimetros(EX: 1.80): "));
+
+let imc = peso/altura**2
+if(imc < 18.5){
+    texto = "Você está abaixo do peso ideal"
+    console.log(texto);
+} else if(imc <= 24.9){
+    texto2 = "Você está no peso ideal"
+    console.log(texto2);
+} else{
+    texto3 = "Você está acima do peso"
+    console.log(texto3)
+}
